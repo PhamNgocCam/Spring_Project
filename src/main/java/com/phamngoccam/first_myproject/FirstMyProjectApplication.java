82509c4837d0ab959daded940ba70f9f89d5eaea
PhamNgocCam <21t1020256@husc.edu.vn>
2024-08-15 03:19:02 +0700

    Spring_project

---
+++ src/main/java/com/phamngoccam/first_myproject/FirstMyProjectApplication.java
@@ -0,0 +1,13 @@
+package com.phamngoccam.first_myproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FirstMyProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FirstMyProjectApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/phamngoccam/first_myproject/FirstMyProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.phamngoccam.first_myproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FirstMyProjectApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
